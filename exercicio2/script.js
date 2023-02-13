6c964f2368261f4abe66f7e6d53b7c4a8a58613c
Juliano Amaral <jaygovinda13@gmail.com>
2023-02-13 14:49:38 -0300

    revisão strings feita

--- exercicio2/script.js
+++ exercicio2/script.js
@@ -1,1 +1,4 @@
 const minhaString = "Eu não sou supersticioso, mas sou um pouco ________.      ";
+console.log(minhaString.trim())
+const novaString = minhaString.replace("________", 'sticioso' )
+console.log(novaString)
--- exercicio3/script.js
+++ exercicio3/script.js
@@ -1,1 +1,6 @@
 //Crie a const para a frase aqui
+
+const frase = `Jorge tem uma casa verde e com portão azul, com os dizeres: "BOAS VINDAS, mas não deixe o gato sair"`
+const novaFrase = frase.replace(`verde`, `rosa`).replace(`azul`, `laranja`).toUpperCase(`mas não deixe o gato sair`)
+
+console.log(novaFrase)
